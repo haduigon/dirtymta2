515d02e57e368205d9f01bde356832931f372403
haduigon <haduigon@gmail.com>
2024-09-14 23:14:53 +0300

    gallery shuffling added

--- src/components/PagePattern/PagePattern.tsx
+++ src/components/PagePattern/PagePattern.tsx
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Typography, Container, Paper, Box } from '@mui/material';
 
-import {
-  AiFillMessage,
-  AiFillBank,
-} from "react-icons/ai";
+// import {
+//   AiFillMessage,
+//   AiFillBank,
+// } from "react-icons/ai";
 import { FaGoogle } from "react-icons/fa";
-import { TbBrandYahoo } from "react-icons/tb";
-import { MdAlternateEmail, MdMarkEmailRead, MdEuro, MdOutlineTextsms } from "react-icons/md";
-import { SiGmail } from "react-icons/si";
-import { LuDollarSign } from "react-icons/lu";
-import { IoLogoBitcoin } from "react-icons/io";
-import { PiCrown } from "react-icons/pi";
+// import { TbBrandYahoo } from "react-icons/tb";
+// import { MdAlternateEmail, MdMarkEmailRead, MdEuro, MdOutlineTextsms } from "react-icons/md";
+// import { SiGmail } from "react-icons/si";
+// import { LuDollarSign } from "react-icons/lu";
+// import { IoLogoBitcoin } from "react-icons/io";
+// import { PiCrown } from "react-icons/pi";
 import { IconBaseProps } from 'react-icons';
+// import {shuffledIconsArray} from '../../data/iconsArray';
+import {iconsLink} from '../../data/iconsArray';
 
 
 type Props = {
   content?: string | React.ReactNode,
   name?: string,
-  icons?: React.ComponentType<IconBaseProps>[];
+  icons?: {
+    component: React.ComponentType<IconBaseProps>,
+    size: number,
+    color: string,
+    id: number,
+  }[];
+}
+
+function shuffle(array: any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+  }
+  return array;
 }
 
 function PagePattern({ name, content, icons }: Props): JSX.Element {
+  const [randomIcons, setRandomIcons] = useState(iconsLink);
+
+  useEffect(() => {
+    const shfld = shuffle(iconsLink)
+    setRandomIcons(shfld);
+  }, []);
+
   return (
     <Container
       sx={{
@@ -46,27 +68,38 @@ function PagePattern({ name, content, icons }: Props): JSX.Element {
             justifyContent: 'space-between'
           }}
         >
-          <Box
-            sx={{
-              // marginRight: '5%'
-            }}
-          >
-            <AiFillMessage size={38} color='lightblue' />
-          </Box>
 
-          <Box>
-            <AiFillBank size={40} color='wheat' />
+          {randomIcons.map((icon, indx) => {
+            const IconComponent = icon.component;
+            return (
+              <Box
+                sx={{
+                  display: {
+                    xs: indx < 8 ? 'none': 'flex',
+                    sm: indx < 8 ? 'none': 'flex',
+                    md: 'flex'
+                  },
+                  alignItems: 'center',
+                }}
+                key={icon.id}
+          >
+            <IconComponent size={icon.size} color={icon.color} />
           </Box>
+            )
+          })}
+          {/* <Box>
+            <AiFillBank size={40} color='wheat' />
+          </Box> */}
           
-          <Box
+          {/* <Box
             sx={{
               display: 'flex',
               alignItems: 'center'
             }}
           >
             <FaGoogle size={32} color='orange' />
-          </Box>
-         
+          </Box> */}
+{/*          
           <Box
             sx={{
               display: 'flex',
@@ -146,7 +179,7 @@ function PagePattern({ name, content, icons }: Props): JSX.Element {
             }}
           >
             <PiCrown size={40} color='orange' />
-          </Box>
+          </Box> */}
 
 
         </Box>
--- src/data/iconsArray.ts
+++ src/data/iconsArray.ts
@@ -11,7 +11,7 @@ import { IoLogoBitcoin } from "react-icons/io";
 import { PiCrown } from "react-icons/pi";
 import { IconBaseProps } from "react-icons";
 
-function shuffle(array: React.ComponentType<IconBaseProps>[]) {
+function shuffle(array: any[]) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]]; // Swap elements
@@ -20,19 +20,19 @@ function shuffle(array: React.ComponentType<IconBaseProps>[]) {
 }
 
 export const iconsLink = [
-  AiFillMessage,
-  AiFillBank,
-  FaGoogle,
-  TbBrandYahoo,
-  MdAlternateEmail,
-  MdMarkEmailRead,
-  MdEuro,
-  MdOutlineTextsms,
-  SiGmail,
-  LuDollarSign,
-  IoLogoBitcoin,
-  PiCrown,
+  {component: AiFillMessage, size: 38, color: 'lightblue', id: 1},
+  {component: AiFillBank, size: 40, color: 'wheat', id: 2},
+  {component: FaGoogle, size: 32, color: 'orange', id: 3},
+  {component: TbBrandYahoo, size: 40, color: 'purple', id: 4},
+  {component: MdAlternateEmail, size: 40, color: 'pink', id: 5},
+  {component: MdMarkEmailRead, size: 40, color: 'lightgreen', id: 6},
+  {component: SiGmail, size: 38, color: 'darkyellow', id: 7},
+  {component: LuDollarSign, size: 40, color: 'green', id: 8},
+  {component: MdEuro, size: 38, color: 'blue', id: 9},
+  {component: IoLogoBitcoin, size: 40, color: 'darkgrey', id: 10},
+  {component: MdOutlineTextsms, size: 40, color: 'purple', id: 11},
+  {component: PiCrown, size: 40, color: 'orange', id: 12},
 ];
 
-export const shuffledIconsArray = shuffle(iconsLink); 
+// export const shuffledIconsArray = shuffle(iconsLink); 
 
