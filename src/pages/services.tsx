6f38a22559c44d96a8b2ce31111cb4343752dfd6
haduigon <haduigon@gmail.com>
2024-10-23 17:28:25 +0300

    Content added

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PagePattern from '@/components/PagePattern';
+import { servicesText } from '@/assets/text/text';
 
 export default function Services() {
   return (
-    <PagePattern content='Welcome to the services Page'/ >
+    <PagePattern content={servicesText} / >
   );
 }
