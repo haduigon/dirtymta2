2181767b6ed69e41577481ccb58d8b6574cdea58
haduigon <haduigon@gmail.com>
2024-10-23 17:34:47 +0300

    Content added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PagePattern from '@/components/PagePattern';
-import { aboutText } from '@/data/pages';
+import { aboutText2 } from '@/data/pages';
 
 export default function Home() {
   return (
-    <PagePattern content={aboutText} name='Who we are'/>
+    <PagePattern content={aboutText2} name='Who we are'/>
   );
 }
